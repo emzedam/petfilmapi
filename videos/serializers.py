92fb4118b8f35cd38abd15e26e64b74deff44ba5
hassan mohammadzadeh <73823169+emzedam@users.noreply.github.com>
2024-10-09 14:43:54 +0330

    latest commit

--- videos/serializers.py
+++ videos/serializers.py
@@ -2,15 +2,70 @@ from rest_framework import serializers
 from categories import serializers as category_serializers
 from categories import models as category_models
 from .models import Video
+import jdatetime
 
 class VideoSerializer(serializers.ModelSerializer):
 
 
     category = serializers.PrimaryKeyRelatedField(queryset=category_models.Category.objects.all() , required=False, allow_null=True)
-    
+    related_videos = serializers.SerializerMethodField()
+    created_at_formatted = serializers.SerializerMethodField()
+
+
     class Meta:
         model = Video
-        fields = '__all__'
+        fields = [
+            "id",
+            "category",
+            "title_fa",
+            "title_en",
+            "description",
+            "uploaded_at",
+            "video_file",
+            "cover",
+            "hls_link",
+            "is_banner",
+            "created_at",
+            "created_at_formatted",
+            "updated_at",
+            "keywords",
+            "seo_description",
+            "release_year",
+            "slug",
+            "related_videos"
+        ]
+        
+    def get_related_videos(self, obj):
+        # گرفتن ویدیوهای مرتبط از context
+        related_videos = self.context.get('related_videos', [])
+        return VideoSerializer(related_videos, many=True).data    
+        
+    def get_created_at_formatted(self, obj):
+        # تبدیل تاریخ میلادی به شمسی
+        gregorian_date = obj.created_at
+        jalali_date = jdatetime.datetime.fromgregorian(datetime=gregorian_date)
+
+
+        months_persian = {
+            'Farvardin': 'فروردین',
+            'Ordibehesht': 'اردیبهشت',
+            'Khordad': 'خرداد',
+            'Tir': 'تیر',
+            'Mordad': 'مرداد',
+            'Shahrivar': 'شهریور',
+            'Mehr': 'مهر',
+            'Aban': 'آبان',
+            'Azar': 'آذر',
+            'Dey': 'دی',
+            'Bahman': 'بهمن',
+            'Esfand': 'اسفند'
+        }
+
+        month_english = jalali_date.strftime('%B')
+        
+        month_persian = months_persian.get(month_english, month_english)
+
+        return f'{jalali_date.year} {month_persian} {jalali_date.day}'
         
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -18,6 +73,7 @@ class VideoSerializer(serializers.ModelSerializer):
             representation['category'] = {
                 "id": instance.category.id,
                 "title": instance.category.title,
+                "slug": instance.category.slug,
                 "image": instance.category.image.url if instance.category.image else None
             }
         return representation
--- videos/views/front.py
+++ videos/views/front.py
@@ -8,3 +8,27 @@ class FrontVideoViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = VideoSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['title_fa' , 'title_en']
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+
+        # بررسی اینکه آیا شناسه ویدیو در URL موجود است
+        video_id = self.kwargs.get('pk')  # یا استفاده از 'slug' در صورت استفاده از URL slug
+        if video_id:
+            try:
+                # گرفتن ویدیو فعلی
+                current_video = self.get_object()
+
+                # گرفتن 10 ویدیو از همان دسته بندی
+                related_videos = Video.objects.filter(
+                    category=current_video.category
+                ).exclude(id=current_video.id)[:10]
+
+                # اضافه کردن ویدیوهای مشابه به context
+                context['related_videos'] = related_videos
+            except Video.DoesNotExist:
+                context['related_videos'] = []  # اگر ویدیو وجود نداشت، یک لیست خالی
+        else:
+            context['related_videos'] = []  # اگر ID وجود نداشت، یک لیست خالی
+
+        return context
